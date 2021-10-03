655c3ec6e7525e2c1902e1cc2767f56cfe7edad7
rabiulkhan07 <rabiulkhanofficial@gmail.com>
2021-10-03 23:22:11 +0600

    Basic structure of login form

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -3,9 +3,32 @@ import React from 'react';
 const Login = () => {
     return (
         <>
-            <div className = "">
+        <div className="">
+            <div className = "row">
+                <div className="col-md-4"></div>
+                <div className="col-md-4 shadow-lg p-3 mb-5 bg-white rounded center-screen">
+                    <h1>Login</h1>
+                    <form>
+                        <div className="mb-3">
+                            <label for="exampleInputEmail1" className="form-label">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div className="mb-3">
+                            <label for="exampleInputPassword1" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" />
+                        </div>
+                        <div className="mb-3 form-check">
+                            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                            <label className="form-check-label" for="exampleCheck1">Remember me </label>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+                <div className="col-md-4"></div>
                 
             </div>
+        </div>
         </>
     );
 };
